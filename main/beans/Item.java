a347963eee5ad8ce6a4296ed84ee9df4c5fd1e45
Srigowri321 <62496576+Srigowri321@users.noreply.github.com>
2021-09-08 22:16:05 +0530

    Update Item.java
    Updated item.java

--- main/beans/Item.java
+++ main/beans/Item.java
@@ -11,7 +11,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.sun.istack.NotNull;
+import javax.validation.constraints.NotNull;
+
+
 
 
 
@@ -28,13 +30,18 @@ public class Item {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long itemId;
 	@Column
-	private String name;
+	@NotNull(message="Price should not be empty")
+	private Double price;
 	@Column
-	private String color;
-	@Column
+	@NotNull
 	private String category;
 	@Column
+	@NotNull(message="Color should not be empty")
+	private String color;
+	@NotNull(message="Quantity should not be empty")
+	@Column
 	private Integer quantity;
+	
 	@Column
 	private String material;
 	@Column
@@ -42,31 +49,29 @@ public class Item {
 	
 	public Item() {}
 
-//	public Item(Long itemId, @NotNull(message="Name should not be empty") String name, @NotNull(message="Color should not be empty")String color,String category,@NotNull(message="Quantity should not be empty")Integer quantity,String material,String description) {
-//	this.itemId=itemId;
-//	this.name=name;
-//	this.color=color;
-//	this.quantity = quantity;
-//	this.material = material;
-//	this.description = description;
-//	}
+	public Item(Long itemId,  @NotNull(message="Color should not be empty")String color,String category,@NotNull(message="Quantity should not be empty")Integer quantity,String material,String description) {
+	this.itemId=itemId;
+	this.color=color;
+	this.category=category;
+	this.quantity = quantity;
+	this.material = material;
+	this.description = description;
 	
-	public Item(Long itemId, String name, String color, String category, Integer quantity, String material, String description) {
+	}
+
+	/*public Item( String category,String color, Integer quantity, String material, String description) {
 		super();
 		this.itemId = itemId;
-		this.name = name;
 		this.color = color;
 		this.category = category;
 		this.quantity = quantity;
 		this.material = material;
 		this.description = description;
 	}
+*/
 
-
-	public void UpdateIold( String name, String color, String category, Integer quantity, String material,
+	public void UpdateIold( String category, String color,Integer quantity, String material,
 	String description) {
-	
-		this.name = name;
 		this.color = color;
 		this.category = category;
 		this.quantity = quantity;
@@ -90,14 +95,7 @@ public class Item {
 		this.itemId = itemId;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
+	
 	public String getColor() {
 		return color;
 	}
@@ -140,15 +138,34 @@ public class Item {
 
 	@Override
 	public String toString() {
-		return "Item [itemId=" + itemId + ", name=" + name + ", color=" + color + ", category=" + category
+		return "Item [itemId=" + itemId + ", color=" + color + ", category=" + category
 				+ ", quantity=" + quantity + ", material=" + material + ", description=" + description + "]";
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice() {
+		
+		switch(this.category)
+		{
+		case "shirt" : this.price=10.0*this.quantity;     		 break;
+		case "saree-silk" : this.price=49.0*this.quantity;		 break;
+		case "Designer" : this.price=79.0*this.quantity;	     break;
+		case "Gents-Kurta" : this.price=8.0*this.quantity;		 break;
+		case "Ladies-Kurta" : this.price=8.0*this.quantity;		 break;
+		case "Blazer-jacket" : this.price=10.0*this.quantity;    break;
+		case "Bedsheet-Double" : this.price=29.0*this.quantity;  break;
+		case "pant" : this.price=12.0*this.quantity;		     break;
+		default: this.price=0.0*this.quantity;
+		}
 	}
 
 	
 	/*public Customer getUserId() {
 		return userId;
 	}
-
 	public void setUserId(Customer userId) {
 		this.userId = userId;
 	}*/
@@ -159,4 +176,12 @@ public class Item {
 	
 
 }
+
+	
+	
+	
+	
+	
+
+
 
